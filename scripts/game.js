7c4de9450686c3db52a399dcfdccab825e408120
Jacob Welsh <welshy92@outlook.com>
2023-01-30 15:33:57 +0000

    Add newGame, showScore & addTurn functions

--- scripts/game.js
+++ scripts/game.js
@@ -5,5 +5,22 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 };
 
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+    addTurn();
+};
 
-module.exports = { game };
+function showScore() {
+    document.getElementById("score").innerText = game.score
+};
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurn();
+};
+
+module.exports = { game, newGame, showScore, addTurn };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 
 beforeAll(() => {
@@ -13,20 +13,42 @@ beforeAll(() => {
     document.close();
 });
 
-describe("game object contains correct keys", () => {
-    test("score key exists", () => {
+describe("Game object contains correct keys", () => {
+    test("Score key exists", () => {
         expect("score" in game).toBe(true);
     });
-    test("currentGame key exists", () => {
+    test("CurrentGame key exists", () => {
         expect("currentGame" in game).toBe(true);
     });
-    test("playerMoves key exists", () => {
+    test("PlayerMoves key exists", () => {
         expect("playerMoves" in game).toBe(true);
     });
-    test("choices key exists", () => {
+    test("Choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
-    test("choices contain correct ids", () => {
+    test("Choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+});
+
+describe("newGame function works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2", "button3"];
+        game.currentGame = ["button1", "button2", "button3"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    })
+    test("Score resets to 0", () => {
+        expect(game.score).toEqual(0);
+    })
+    test("playerMoves to be cleared", () => {
+        expect(game.playerMoves).toStrictEqual([]);
+    })
+    test("should be 1 element in computers array", () => {
+        expect(game.currentGame.length).toBe(1);
+    })
+    test("Element with id of score should display 0", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    })
 });
